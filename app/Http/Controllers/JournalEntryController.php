9d8d8d55207b1b5d9acabe811a63b4ab16b6bcbf
bendaryy <mohamedbendary1994@gmail.com>
2023-05-11 14:30:08 +0300

    kdsjfs

--- app/Http/Controllers/JournalEntryController.php
+++ app/Http/Controllers/JournalEntryController.php
@@ -171,6 +171,13 @@ class JournalEntryController extends Controller
             $accounts->prepend('--', '');
 
             return view('journalEntry.edit', compact('accounts', 'journalEntry'));
+
+        } else if (\Auth::user()->type == 'super admin') {
+            $accounts = ChartOfAccount::select(\DB::raw('CONCAT(code, " - ", name) AS code_name, id'))->where('created_by', $journalEntry->created_by)->get()->pluck('code_name', 'id');
+            $accounts->prepend('--', '');
+
+            return view('journalEntry.edit', compact('accounts', 'journalEntry'));
+
         } else {
             return response()->json(['error' => __('Permission denied.')], 401);
         }
@@ -236,10 +243,68 @@ class JournalEntryController extends Controller
             } else {
                 return redirect()->back()->with('error', __('Permission denied.'));
             }
+        } elseif (\Auth::user()->type == 'super admin') {
+
+            $validator = \Validator::make(
+                $request->all(), [
+                    'date' => 'required',
+                    'accounts' => 'required',
+                ]
+            );
+            if ($validator->fails()) {
+                $messages = $validator->getMessageBag();
+
+                return redirect()->back()->with('error', $messages->first());
+            }
+
+            $accounts = $request->accounts;
+
+            $totalDebit = 0;
+            $totalCredit = 0;
+            for ($i = 0; $i < count($accounts); $i++) {
+                $debit = isset($accounts[$i]['debit']) ? $accounts[$i]['debit'] : 0;
+                $credit = isset($accounts[$i]['credit']) ? $accounts[$i]['credit'] : 0;
+                $totalDebit += $debit;
+                $totalCredit += $credit;
+            }
+
+            if ($totalCredit != $totalDebit) {
+                return redirect()->back()->with('error', __('Debit and Credit must be Equal.'));
+            }
+
+            $journalEntry->date = $request->date;
+            $journalEntry->reference = $request->reference;
+            $journalEntry->description = $request->description;
+            $journalEntry->Approve = 1;
+            // $journalEntry->created_by = \Auth::user()->creatorId();
+            $journalEntry->save();
+
+            for ($i = 0; $i < count($accounts); $i++) {
+                $journalItem = JournalItem::find($accounts[$i]['id']);
+
+                if ($journalItem == null) {
+                    $journalItem = new JournalItem();
+                    $journalItem->journal = $journalEntry->id;
+                }
+
+                if (isset($accounts[$i]['account'])) {
+                    $journalItem->account = $accounts[$i]['account'];
+                }
+
+                $journalItem->description = $accounts[$i]['description'];
+                $journalItem->debit = isset($accounts[$i]['debit']) ? $accounts[$i]['debit'] : 0;
+                $journalItem->credit = isset($accounts[$i]['credit']) ? $accounts[$i]['credit'] : 0;
+                $journalItem->save();
+            }
+
+            return redirect()->route('approvedjournal')->with('success', __('Journal entry successfully updated and waiting for admin approval.'));
+
         } else {
             return redirect()->back()->with('error', __('Permission denied.'));
         }
     }
+
+    // approve event for admin
 
     public function adminApprove($id)
     {
